d9a1c6f34160308612a62a4af77bd20812308793
PranayKrishnak <pranaykrishna1143@gmail.com>
2025-01-06 19:37:44 +0530

    floders adddes

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Pranay KK',
+    author_email= 'shanthit841@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
